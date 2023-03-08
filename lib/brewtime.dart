1bbeb40c3769836a71e380f26ff24aa710baba87
mswift42 <martin.haesler@gmail.com>
2023-03-08 08:49:35 +0000

    add factory methods

--- lib/brewtime.dart
+++ lib/brewtime.dart
@@ -1,5 +1,10 @@
 class BrewTime extends Duration {
   final int minutes;
   final int seconds;
+
   const BrewTime({required this.minutes, required this.seconds});
+
+  factory BrewTime.fromJson(Map<String, dynamic> json) {
+    return BrewTime(minutes: json['minutes'], seconds: json['seconds']);
+  }
 }
--- lib/filterpaper.dart
+++ lib/filterpaper.dart
@@ -2,4 +2,8 @@ class FilterPaper {
   final String name;
 
   const FilterPaper({required this.name});
+
+  factory FilterPaper.fromJson(Map<String, dynamic> json) {
+    return FilterPaper(name: json['na']);
+  }
 }
